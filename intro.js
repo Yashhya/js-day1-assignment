30bbb32b5430a50bf577dc9c944c04306412a13b
Yash Yadav <yash9648444689@gmail.com>
2025-12-09 11:41:42 +0000

    added intro.js

--- intro.js
+++ intro.js
@@ -2,7 +2,7 @@
 
 // Basic introduction program
 console.log("Hello, this is my first JavaScript program using GitHub Codespaces!");
-console.log("Name: Kushagra Singh Chandel");
+console.log("Name: Yash Yadav");
 console.log("College: United College Of Engineering and Research");
 console.log("Course: B.Tech CSE (AIML)");
 console.log("Today I am learning JavaScript basics and GitHub workflow.");
